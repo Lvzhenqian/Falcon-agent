80dfecd1ffbc8a333295e7bd635d467d5181a585
lv <lv@qq.com>
2017-07-14 19:47:35 +0800

    abc

--- AppMain.py
+++ AppMain.py
@@ -1,36 +1,42 @@
-import os
+# coding=utf-8
 import logging
-from util import thread
-from util.config import log_File, console, leve
-from AutoUpdate import Install
-from urllib import request
+import os
+import sys
+
+import AutoInstall
+import crond
+from config import log_File, leve, coding, HTTP
 
 main_log = logging.getLogger('root')
 main_log.setLevel(leve)
 main_log.addHandler(log_File)
-main_log.addHandler(console)
+# main_log.addHandler(console)
+main_log.debug('console coding is: %s' % sys.getdefaultencoding())
+coding()
 
 
 def service():
-    main_log.debug('开启API服务!')
-    thread.APIthread()
-    main_log.debug('开始作业：{}'.format(thread.Jobs.get_jobs()))
+    if HTTP.get('enabled'):
+        main_log.debug(u'开启API服务!')
+        crond.APIthread()
+    main_log.debug(u'开始作业：%s' % (crond.Jobs.get_jobs()))
     try:
-        thread.Jobs.start()
+        crond.Jobs.start()
     except (KeyboardInterrupt, SystemExit):
-        thread.Jobs.shutdown()
-        main_log.debug('主进程退出！')
+        crond.Jobs.shutdown()
+        main_log.debug(u'主进程退出！')
 
 
 def main():
-    m = Install.Upgrade()
+    m = AutoInstall.Upgrade()
     if not os.path.exists(m.Install_Path) or os.path.exists(m.AgentMd5):
-        with request.urlopen(m.AgentMd5) as f:
-            md5file = f.read()
-        return m.Download_And_Install(md5file)
+        main_log.debug(u"检查到目录或者MD5文件不存在，开始安装程序！")
+        m.Download_And_Install(m.Remote_Md5())
     else:
+        main_log.debug(u"程序已存在开启后台进程服务！")
         service()
 
 
 if __name__ == '__main__':
+    # service()
     main()
